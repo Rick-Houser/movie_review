e0b95b068bef15c578197d820df8a005d7d1a8cc
FrackenToaster <frakentoaster@gmail.com>
2015-09-21 21:05:06 -0700

    Add aws configuration to environments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,4 +41,8 @@ Rails.application.configure do
   # 
   # Required by devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :bucket => 'web-folio'
+  }
 end
